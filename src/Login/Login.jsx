af191097fca3adb9a4048663b8c23d8714642a90
Nitesh Kumar Pandey <niteshpandit676@gmail.com>
2022-09-30 09:03:18 +0530

    login div tested

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -6,6 +6,12 @@ function Login() {
         <label for="username-input">Username</label>
         <input id="username-input" />
 
+        {/* for getting input from test-cases */}
+
+        <div data-testid="div-id">
+            <h2>this is diffrent scenrios of test-cases</h2>
+        </div>
+
     </div>
   )
 }
--- src/Login/__test__/login.test.js
+++ src/Login/__test__/login.test.js
@@ -1,10 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import Login from '../Login';
 
-test('should show login form', () => {
-    render(<Login />)
-    const input = screen.getByLabelText('Username')
-    // console.log(input)
-    expect(input).toBeInTheDocument();
-
-  })
+describe("Login Component ", () =>{
+    it('should show login form', () => {
+        render(<Login />)
+        const input = screen.getByLabelText('Username')
+        // console.log(input)
+        expect(input).toBeInTheDocument();
+    
+      });
+    
+    ///test cases on the behalf of id 
+    
+    it("test cases on the behalf of id",()=>{
+        render(<Login />);
+        const element = screen.getByTestId('div-id');
+        expect(element).toBeInTheDocument();
+    
+    
+    })
+})
