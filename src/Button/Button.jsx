1fad0fb5d138b7f0a14fbce3d7d006431f1fd58d
Nitesh Kumar Pandey <niteshpandit676@gmail.com>
2022-09-30 08:20:08 +0530

    code commented

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 function ButtonDiv() {
   return (
+    <>
     <div>
-        <button alt="test-check" className='test-karo' >click the button</button>
+        <button alt="btn-test" className='test-karo' >click the button</button>
+        <p>this is paragraph</p>
     </div>
+
+
+    <main aria-hidden="true">
+    <button>Open dialog</button>
+  </main>
+  <div role="dialog">
+    <button>Close dialog</button>
+  </div>
+    </>
   )
 }
 
--- src/Button/__test__/button.test.js
+++ src/Button/__test__/button.test.js
@@ -1,13 +1,25 @@
 import { render, screen } from '@testing-library/react';
-import Button from '../Button';
+import ButtonDiv from '../Button';
 
 
-describe("button test-cases", () =>{
+describe("button component  test-cases", () =>{
     it('button test cases',async()=>{
-        render(<Button/>);
-        const containButton = await screen.findAllByRole('button');
+        render(<ButtonDiv/>);
+        // const containButton = await screen.getByRole('button')
         // console.log(containButton);
-        expect(containButton).toBeInTheDocument();
+        // expect(containButton).toBeInTheDocument();
+        // expect(containButton).toBeInTheDOM()
         
     })
+
+    it('p test cases',async()=>{
+        render(<ButtonDiv/>);
+        const containButton = await screen.getAllByRole('button', { hidden: true })
+        console.log(containButton);
+        // expect(containButton).toBeInTheDocument();
+        // expect(containButton).toBeInTheDOM()
+        
+    })
+
+
 })
